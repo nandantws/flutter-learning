bdfe200ad684ba6fc3adc9da2efbc2be5188815b
dalbeertws1 <dalbeer.tws@gmail.com>
2022-06-30 16:05:52 +0530

    dyanmic categories

--- lib/screens/home/categories.dart
+++ lib/screens/home/categories.dart
@@ -2,23 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:helloworld/screens/home/utils.dart';
 
 class ProductsInGridView extends StatelessWidget {
-  String text = '';
+  String brand = '';
   List _products = [
+    // {
+    //   'brand': 'All',
+    //   'category': 'Unisex Shoes',
+    //   'name': 'Balenciaga Speed',
+    //   'image': 'assets/balenciaga.png',
+    //   'colors': [Colors.black, Colors.cyan, Colors.teal],
+    //   'price': 485,
+    // },
     {
-      'category': 'Unisex Shoes',
-      'name': 'Balenciaga Speed',
-      'image': 'assets/balenciaga.png',
-      'colors': [Colors.black, Colors.cyan, Colors.teal],
-      'price': 485,
-    },
-    {
-      'category': "Men's Shoes",
-      'name': 'Nike Zoom Freak',
-      'image': 'assets/nike.png',
-      'colors': [Colors.black, Colors.green],
-      'price': 485,
-    },
-    {
+      'brand': 'Puma',
       'category': "Men's Sneaker",
       'name': '1DER Vegas',
       'image': 'assets/puma.png',
@@ -26,37 +21,76 @@ class ProductsInGridView extends StatelessWidget {
       'price': 485,
     },
     {
+      'brand': 'Addidas',
       'category': 'Running Shoes',
       'name': 'ADISTEN M',
       'image': 'assets/adidas.png',
       'colors': [Colors.black, Colors.blueAccent, Colors.grey],
       'price': 485,
     },
+    {
+      'brand': 'Nike',
+      'category': "Men's Shoes",
+      'name': 'Nike Air Pegasus',
+      'image': 'assets/nike_air_zoom_pegasus.png',
+      'colors': [Colors.black, Colors.green],
+      'price': 485,
+    },
+    {
+      'brand': 'Nike',
+      'category': "Men's Shoes",
+      'name': 'Nike Air Presto',
+      'image': 'assets/nike_air_presto.png',
+      'colors': [Colors.black, Colors.green],
+      'price': 485,
+    },
+    {
+      'brand': 'Nike',
+      'category': "Men's Shoes",
+      'name': 'Nike Air Force',
+      'image': 'assets/nike_air_force.png',
+      'colors': [Colors.black, Colors.green],
+      'price': 485,
+    },
+    {
+      'brand': 'Nike',
+      'category': "Men's Shoes",
+      'name': 'Nike Zoom Freak',
+      'image': 'assets/nike_zoom_freak.png',
+      'colors': [Colors.black, Colors.green],
+      'price': 485,
+    }
   ];
 
-  ProductsInGridView({required this.text});
+  ProductsInGridView({required this.brand});
 
   @override
   Widget build(BuildContext context) {
     Size _size = MediaQuery.of(context).size;
+    List _brandProducts = [];
+    print(brand);
+    brand == 'All'
+        ? _brandProducts = _products
+        : _brandProducts =
+            _products.where((element) => element['brand'] == brand).toList();
 
     return GridView.builder(
       shrinkWrap: true,
       physics: NeverScrollableScrollPhysics(),
-      itemCount: _products.length,
+      itemCount: _brandProducts.length,
       padding: EdgeInsets.only(bottom: _size.height * 0.02),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           mainAxisSpacing: _size.height * 0.015,
           crossAxisSpacing: _size.height * 0.015,
-          mainAxisExtent: _size.height * 0.372,
+          mainAxisExtent: _size.height * 0.37,
           crossAxisCount: 2),
       itemBuilder: (_, index) {
         return ProductCard(
-          category: _products[index]['category'],
-          name: _products[index]['name'],
-          image: _products[index]['image'],
-          colors: _products[index]['colors'],
-          price: _products[index]['price'],
+          category: _brandProducts[index]['category'],
+          name: _brandProducts[index]['name'],
+          image: _brandProducts[index]['image'],
+          colors: _brandProducts[index]['colors'],
+          price: _brandProducts[index]['price'],
         );
       },
     );
@@ -121,8 +155,11 @@ class _CategoriesState extends State<Categories> {
                                   isSelected: i == selectedIndex ? true : false,
                                 )))
                       ]),
+                  SizedBox(
+                    height: _size.height * 0.01,
+                  ),
                   ProductsInGridView(
-                    text: _categories[selectedIndex],
+                    brand: _categories[selectedIndex],
                   )
                 ],
               )),
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -11,37 +11,37 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // extendBodyBehindAppBar: true,
-        appBar: CustomAppbar(),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Container(
-              color: Color(0xFFF8F8F8),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SizedBox(
-                    height: 20,
-                  ),
-                  NewProductBanner(),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Categories(),
-                ],
-              ),
+      // extendBodyBehindAppBar: true,
+      appBar: CustomAppbar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Container(
+            color: Color(0xFFF8F8F8),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                NewProductBanner(),
+                SizedBox(
+                  height: 20,
+                ),
+                Categories(),
+              ],
             ),
           ),
-        )
-        // bottomNavigationBar: BottomNavigationBar(
-        //     showSelectedLabels: false,
-        //     showUnselectedLabels: false,
-        //     items: [
-        //       BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
-        //       BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
-        //       BottomNavigationBarItem(icon: Icon(Icons.card_travel), label: ''),
-        //     ]),
-        );
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
+            BottomNavigationBarItem(icon: Icon(Icons.card_travel), label: ''),
+          ]),
+    );
   }
 }
